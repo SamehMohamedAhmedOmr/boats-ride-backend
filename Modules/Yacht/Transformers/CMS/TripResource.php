a9fc8b3c5719486abe57af2447d5c464af4704cf
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2021-07-02 15:47:30 +0200

    Update TripResource.php

--- Modules/Yacht/Transformers/CMS/TripResource.php
+++ Modules/Yacht/Transformers/CMS/TripResource.php
@@ -2,54 +2,54 @@
 
 namespace Modules\Yacht\Transformers\CMS;
 
+use Illuminate\Http\Request;
 use Modules\Yacht\Enums\TripStatusEnum;
 use Modules\Yacht\Enums\PaymentMethodsEnum;
 use Modules\Base\Transformers\CountryResource;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Modules\Yacht\Transformers\CMS\YachtResource;
-use Modules\Yacht\Transformers\CMS\TripClientResource;
 
 class TripResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request
+     * @param Request
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'id'=>$this->id,
-            'start_hour'=>$this->start_hour,
-            'start_date'=>$this->start_date,
-            'end_hour'=>$this->end_hour,
-            'end_date'=>$this->end_date,
-            'status'=>$this->status,
-            'status_name'=> TripStatusEnum::getKey($this->status),
-            'payment_method'=>$this->payment_method,
-            'payment_method_name'=>PaymentMethodsEnum::getKey($this->payment_method),
+            'id' => $this->id,
+            'start_hour' => $this->start_hour,
+            'start_date' => $this->start_date,
+            'end_hour' => $this->end_hour,
+            'end_date' => $this->end_date,
+            'status' => $this->status,
+            'phone' => $this->phone,
+            'status_name' => TripStatusEnum::getKey($this->status),
+            'payment_method' => $this->payment_method,
+            'payment_method_name' => PaymentMethodsEnum::getKey($this->payment_method),
             // 'client'=> $this->whenLoaded('client',function(){
             //     return new TripClientResource($this->client);
             // }),
-            'number_of_people'=>$this->number_of_people,
-            'rate_per_hour'=>$this->rate_per_hour,
-            'other_changes'=>(double) $this->other_changes,
-            'discount'=>(double)$this->discount,
-            'minimum_Advance_Payment'=>(double) $this->minimum_Advance_Payment,
-            'coupon_code'=>$this->coupon_code,
-            'client_notes'=>$this->client_notes,
-            'admin_notes'=>$this->admin_notes,
-            'trip_duration'=>$this->trip_duration,
-            'name'=>$this->name,
-            'email'=>$this->email,
-            'title'=>$this->title,
-            'address'=>$this->address,
-            'booking_number'=>$this->booking_number,
-            'country'=> $this->whenLoaded('country',function(){
+            'number_of_people' => $this->number_of_people,
+            'rate_per_hour' => $this->rate_per_hour,
+            'other_changes' => (double)$this->other_changes,
+            'discount' => (double)$this->discount,
+            'minimum_Advance_Payment' => (double)$this->minimum_Advance_Payment,
+            'coupon_code' => $this->coupon_code,
+            'client_notes' => $this->client_notes,
+            'admin_notes' => $this->admin_notes,
+            'trip_duration' => $this->trip_duration,
+            'name' => $this->name,
+            'email' => $this->email,
+            'title' => $this->title,
+            'address' => $this->address,
+            'booking_number' => $this->booking_number,
+            'country' => $this->whenLoaded('country', function () {
                 return CountryResource::make($this->country);
-             }),
-            'yacht'=> $this->whenLoaded('yacht',function(){
+            }),
+            'yacht' => $this->whenLoaded('yacht', function () {
                 return new YachtResource($this->yacht);
             }),
         ];
