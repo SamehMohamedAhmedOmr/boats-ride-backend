6d96a0748bf16e50c3afb306a21591108b324028
mustafastef2020 <mustafamammdoh@gmail.com>
2021-07-10 23:53:28 +0200

    fix water sport time slots

--- Modules/WaterSports/Http/Requests/CMS/WaterSportTimeslotsRequest.php
+++ Modules/WaterSports/Http/Requests/CMS/WaterSportTimeslotsRequest.php
@@ -14,7 +14,7 @@ class WaterSportTimeslotsRequest extends FormRequest
     public function rules()
     {
         return [
-            'date'=>'date_format:Y-m-d|after_or_equal:'.now()->format('Y-m-d'),
+            'date'=>'date_format:Y-m-d',
             'water_sport_id'=>'required|integer|exists:water_sports,id'
         ];
     }
